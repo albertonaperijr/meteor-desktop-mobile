27dc8400b745e07dfbeb130faca05372898b0868
Alberto Naperi Jr <albertonaperijr@gmail.com>
2016-04-04 20:37:44 +0800

    Deleted README.mds and modified package.js

--- packages/app-mobile/package.js
+++ packages/app-mobile/package.js
@@ -1,24 +1,34 @@
 Package.describe({
   name: 'app-mobile',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  summary: 'MyApp mobile package'
 });
 
 Package.onUse(function(api) {
+  /* Add our packages that we depend on on both mobile/desktop sides */
   api.versionsFrom('1.3');
-  api.use('ecmascript');
-  api.mainModule('app-mobile.js');
+  api.use([
+    'ecmascript'
+  ],['client','server']);
+
+  /* Add client side dependencies */
+  api.use([
+    'jquery',
+    'angular',
+    'driftyco:ionic',
+    'momentjs:moment',
+    'jasonaibrahim:angular-moment',
+    'danialfarid:ng-file-upload',
+    'okland:camera-ui',
+    'kadira:dochead'
+  ],'client');
+
+  // api.mainModule('app-mobile.js');
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('app-mobile');
-  api.mainModule('app-mobile-tests.js');
-});
+// Package.onTest(function(api) {
+//   api.use('ecmascript');
+//   api.use('tinytest');
+//   api.use('app-mobile');
+//   api.mainModule('app-mobile-tests.js');
+// });
